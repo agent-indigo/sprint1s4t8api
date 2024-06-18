e49e087716fcfe9caffdba8264952864aab911fd
AlexStudent89 <138694949+AlexStudent89@users.noreply.github.com>
2024-06-18 20:40:23 -0230

    Update passenger test file

--- src/test/java/com/keyin/sprint1s4t8/api/classes/models/PassengerModelTest.java
+++ src/test/java/com/keyin/sprint1s4t8/api/classes/models/PassengerModelTest.java
@@ -1,4 +1,36 @@
+//PassengerModelTest
 package com.keyin.sprint1s4t8.api.classes.models;
-public final class PassengerModelTest {
-    public PassengerModelTest() {}
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PassengerModelTest {
+    @Test
+    public void testPassengerCreate() {
+        CityModel cityModel = new CityModel(1, "Montreal", ProvTerrCode.QC);
+        PassengerModel passengerModel = new PassengerModel(1, "Piers", "Anthony", "123-456-7890", cityModel);
+
+        Assertions.assertEquals(1, passengerModel.getId());
+        Assertions.assertEquals("Piers", passengerModel.getFirst());
+        Assertions.assertEquals("Anthony", passengerModel.getLast());
+        Assertions.assertEquals("123-456-7890", passengerModel.getPhone());
+        Assertions.assertEquals(cityModel, passengerModel.getCity());
+    }
+
+    @Test
+    public void testPassengerEdit() {
+        CityModel cityModel = new CityModel(1, "Montreal", ProvTerrCode.QC);
+        PassengerModel passengerModel = new PassengerModel(1, "Piers", "Anthony", "123-456-7890", cityModel);
+
+        passengerModel.setFirst("Danielle");
+        passengerModel.setLast("Steel");
+        passengerModel.setPhone("098-765-4321");
+        CityModel newCityModel = new CityModel(2, "Toronto", ProvTerrCode.ON);
+        passengerModel.setCity(newCityModel);
+
+        Assertions.assertEquals("Danielle", passengerModel.getFirst());
+        Assertions.assertEquals("Steel", passengerModel.getLast());
+        Assertions.assertEquals("098-765-4321", passengerModel.getPhone());
+        Assertions.assertEquals(newCityModel, passengerModel.getCity());
+    }
 }
