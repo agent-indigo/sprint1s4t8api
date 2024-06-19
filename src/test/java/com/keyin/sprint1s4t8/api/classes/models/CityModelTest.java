662b6468bfdfe539ff05b9bd7b6a7f121c3540bf
agent-indigo <bradenhynes@gmail.com>
2024-06-19 10:27:15 -0230

    Added constructor in CityModelTest

--- src/test/java/com/keyin/sprint1s4t8/api/classes/models/CityModelTest.java
+++ src/test/java/com/keyin/sprint1s4t8/api/classes/models/CityModelTest.java
@@ -1,29 +1,19 @@
-// CityModelTest
 package com.keyin.sprint1s4t8.api.classes.models;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import com.keyin.sprint1s4t8.api.enums.ProvTerrCode;
-
-public class CityModelTest {
+public final class CityModelTest {
+    private CityModel city;
+    public CityModelTest() {
+        this.city = new CityModel();
+    }
     @Test
     public void testCityCreate() {
-        CityModel cityModel = new CityModel(1, "Montreal", ProvTerrCode.QC);
-
-        Assertions.assertEquals(1, cityModel.getId());
-        Assertions.assertEquals("Montreal", cityModel.getName());
-        Assertions.assertEquals(ProvTerrCode.QC, cityModel.getProvince());
-    }
-
-    @Test
-    public void testCityEdit() {
-        CityModel cityModel = new CityModel(1, "Montreal", ProvTerrCode.QC);
-
-        cityModel.setName("Toronto");
-        cityModel.setProvince(ProvTerrCode.ON);
-
-        Assertions.assertEquals("Toronto", cityModel.getName());
-        Assertions.assertEquals(ProvTerrCode.ON, cityModel.getProvince());
+        city.setId(1);
+        city.setName("Montreal");
+        city.setProvince(ProvTerrCode.QC);
+        Assertions.assertEquals(1, city.getId());
+        Assertions.assertEquals("Montreal", city.getName());
+        Assertions.assertEquals(ProvTerrCode.QC, city.getProvince());
     }
 }
