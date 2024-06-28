51cf95fddc4049e0ef25fd1fa3a6a838ea612ae9
Manny-Nwokedi <131998414+Manny-Nwokedi@users.noreply.github.com>
2024-06-28 11:52:56 -0230

    upgrade Aircraft files

--- src/main/java/com/keyin/sprint1s4t8/api/classes/controllers/AircraftController.java
+++ src/main/java/com/keyin/sprint1s4t8/api/classes/controllers/AircraftController.java
@@ -1,45 +1,55 @@
+
 package com.keyin.sprint1s4t8.api.classes.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import com.keyin.sprint1s4t8.api.classes.models.AircraftModel;
+import com.keyin.sprint1s4t8.api.classes.models.AirportModel;
+import com.keyin.sprint1s4t8.api.classes.models.PassengerModel;
 import com.keyin.sprint1s4t8.api.classes.services.AircraftService;
+
 @RestController
 @CrossOrigin
+
 public final class AircraftController {
     @Autowired
     private AircraftService aircraftService;
     public AircraftController() {
         this.aircraftService = new AircraftService();
     }
+
     @GetMapping("/aircraft")
     public List<AircraftModel> list() {
         return aircraftService.list();
     }
+
     @GetMapping("/aircraft/{id}")
     public AircraftModel show(@PathVariable int id) {
         return aircraftService.show(id);
     }
+
     @PostMapping("/aircraft")
     public String add(@RequestBody AircraftModel aircraft) {
         return aircraftService.add(aircraft);
     }
+
     @PutMapping("/aircraft/{id}")
-    public String edit(
-        @PathVariable int id,
-        @RequestBody AircraftModel aircraft
-    ) {
+    public String edit(@PathVariable int id, @RequestBody AircraftModel aircraft) {
         return aircraftService.edit(id, aircraft);
     }
+
     @DeleteMapping("/aircraft/{id}")
     public String delete(@PathVariable int id) {
         return aircraftService.delete(id);
     }
 
-    // Set up method that takes in id of aircraft that you will add airport too. Then in the request body it will take in a Airport object of type AirportModel.
-    // Search for aircraft using service method
-    //loop over list of results. Should only be 1 aircraft
-    // If the aircraft is found
-    // In your aircraft object you will have to use the addallowed airports method found there to add the airport
-    // Error Handle Method
+    @PostMapping("/aircraft/{id}/addAirport")
+    public String addAirport(@PathVariable int id, @RequestBody AirportModel airport) {
+        return aircraftService.addAirportToAircraft(id, airport);
+    }
+
+    @PostMapping("/aircraft/{id}/addPassenger")
+    public String addPassenger(@PathVariable int id, @RequestBody PassengerModel passenger) {
+        return aircraftService.addPassengerToAircraft(id, passenger);
+    }
 }
--- src/main/java/com/keyin/sprint1s4t8/api/classes/services/AircraftService.java
+++ src/main/java/com/keyin/sprint1s4t8/api/classes/services/AircraftService.java
@@ -1,30 +1,37 @@
+
 package com.keyin.sprint1s4t8.api.classes.services;
 import org.springframework.stereotype.Service;
 import com.keyin.sprint1s4t8.api.classes.models.AircraftModel;
+import com.keyin.sprint1s4t8.api.classes.models.AirportModel;
+import com.keyin.sprint1s4t8.api.classes.models.PassengerModel;
 import org.springframework.web.server.MethodNotAllowedException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 @Service
+
 public final class AircraftService {
     private Map<Integer, AircraftModel> aircraftMap;
     public AircraftService() {
-        this.aircraftMap = new HashMap<Integer, AircraftModel>();
+        this.aircraftMap = new HashMap<>();
     }
+
     public List<AircraftModel> list() {
         return List.copyOf(aircraftMap.values());
     }
+
     public AircraftModel show(int id) {
         return aircraftMap.get(id);
     }
+
     public String add(AircraftModel aircraft) {
         aircraftMap.put(aircraftMap.size() + 1, aircraft);
         return "Aircraft successfully added.";
     }
-    public String edit(
-        int id,
-        AircraftModel update
-    ) {
+
+    public String edit(int id, AircraftModel update) {
+
         try {
             aircraftMap.put(id, update);
             return "Aircraft successfully updated.";
@@ -32,6 +39,7 @@ public final class AircraftService {
             return methodNotAllowedException.getStackTrace().toString();
         }
     }
+
     public String delete(int id) {
         try {
             aircraftMap.remove(id);
@@ -41,6 +49,23 @@ public final class AircraftService {
         }
     }
 
-    // Search method will be needed
-    //
+    public String addAirportToAircraft(int aircraftId, AirportModel airport) {
+        AircraftModel aircraft = aircraftMap.get(aircraftId);
+        if (aircraft != null) {
+            aircraft.addAllowedAirport(airport);
+            return "Airport added to aircraft.";
+        } else {
+            return "Aircraft not found.";
+        }
+    }
+
+    public String addPassengerToAircraft(int aircraftId, PassengerModel passenger) {
+        AircraftModel aircraft = aircraftMap.get(aircraftId);
+        if (aircraft != null) {
+            aircraft.addPassenger(passenger);
+            return "Passenger added to aircraft.";
+        } else {
+            return "Aircraft not found.";
+        }
+    }
 }
